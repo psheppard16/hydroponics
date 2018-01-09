ecbd2da7192aac5c4b76b0161ac36fba3aa8cec2
Preston Sheppard <psheppard16@gmail.com>
2018-01-09 17:51:01 -0500

    Fixed blank and null options on models

--- hydro/models.py
+++ hydro/models.py
@@ -5,69 +5,69 @@ class Data(models.Model):
     def __str__(self):
         return str(self.type) + ": " + str(self.value) + " on: " + str(self.date_time)
 
-    date_time = models.DateTimeField(null=True)
-    type = models.ForeignKey('DataType', null=False, on_delete=True)
-    value = models.DecimalField(decimal_places=2, max_digits=5, null=False)
+    date_time = models.DateTimeField(blank=False)
+    type = models.ForeignKey('DataType', blank=False, on_delete=True)
+    value = models.DecimalField(decimal_places=2, max_digits=5, blank=True, null=True)
 
 class DataType(models.Model):
     def __str__(self):
         return str(self.type)
 
-    type = models.CharField(max_length=20, null=False)
+    type = models.CharField(max_length=20, blank=False)
 
 class RequestType(models.Model):
     def __str__(self):
         return str(self.type)
 
-    type = models.CharField(max_length=20, null=False)
+    type = models.CharField(max_length=20, blank=False)
 
 class Status(models.Model):
     def __str__(self):
         return str(self.status)
 
-    status = models.CharField(max_length=20, null=False)
+    status = models.CharField(max_length=20, blank=False)
 
 DEFAULT_STATUS_ID = 1
 class Request(models.Model):
     def __str__(self):
         return str(self.type) + " on: " + str(self.request_time)
 
-    request_time = models.DateTimeField(null=True, default=tz.now)
-    exec_time = models.DateTimeField(null=True)
-    type = models.ForeignKey('DataType', null=False, on_delete=True)
-    status = models.ForeignKey('Status', default=DEFAULT_STATUS_ID, null=False, on_delete=True)
-    value = models.DecimalField(decimal_places=2, max_digits=5, null=True)
+    request_time = models.DateTimeField(blank=True, default=tz.now)
+    exec_time = models.DateTimeField(blank=True, null=True)
+    type = models.ForeignKey('RequestType', blank=False, on_delete=True)
+    status = models.ForeignKey('Status', default=DEFAULT_STATUS_ID, blank=False, on_delete=True)
+    value = models.DecimalField(decimal_places=2, max_digits=5, blank=True, null=True)
 
 class Configuration(models.Model):
     #PH REGULATION
-    auto_regulate_pH = models.BooleanField(default=False, null=False)
-    low_pH = models.DecimalField(decimal_places=2, max_digits=5, null=False)
-    high_pH = models.DecimalField(decimal_places=2, max_digits=5, null=False)
-    pH_adj_volume = models.DecimalField(decimal_places=2, max_digits=5, null=False)
-    last_pH_change = models.DateTimeField(null=True)
+    auto_regulate_pH = models.BooleanField(default=False, blank=False)
+    low_pH = models.DecimalField(decimal_places=2, max_digits=5, blank=False)
+    high_pH = models.DecimalField(decimal_places=2, max_digits=5, blank=False)
+    pH_adj_volume = models.DecimalField(decimal_places=2, max_digits=5, blank=False)
+    last_pH_change = models.DateTimeField(null=True, blank=True)
 
     #NUTRIENT REGULATION
-    auto_regulate_nutrients = models.BooleanField(default=False, null=False)
-    low_EC = models.DecimalField(decimal_places=2, max_digits=5, null=False)
-    high_EC = models.DecimalField(decimal_places=2, max_digits=5, null=False)
-    nutrient_adj_volume = models.DecimalField(decimal_places=2, max_digits=5, null=False)
-    last_nutrient_change = models.DateTimeField(null=True)
+    auto_regulate_nutrients = models.BooleanField(default=False, blank=False)
+    low_EC = models.DecimalField(decimal_places=2, max_digits=5, blank=False)
+    high_EC = models.DecimalField(decimal_places=2, max_digits=5, blank=False)
+    nutrient_adj_volume = models.DecimalField(decimal_places=2, max_digits=5, blank=False)
+    last_nutrient_change = models.DateTimeField(null=True, blank=True)
 
     #WASTE REGULATION
-    auto_water_change = models.BooleanField(default=False, null=False)
-    low_ORP = models.DecimalField(decimal_places=2, max_digits=5, null=False)
-    high_ORP = models.DecimalField(decimal_places=2, max_digits=5, null=False)
-    last_water_change = models.DateTimeField(null=True)
-    maximum_water_change_interval = models.IntegerField(default=31, null=False) #days
-    minimum_water_change_interval = models.IntegerField(default=1, null=False) #days
+    auto_water_change = models.BooleanField(default=False, blank=False)
+    low_ORP = models.DecimalField(decimal_places=2, max_digits=5, blank=False)
+    high_ORP = models.DecimalField(decimal_places=2, max_digits=5, blank=False)
+    last_water_change = models.DateTimeField(null=True, blank=True)
+    maximum_water_change_interval = models.IntegerField(default=31, blank=False) #days
+    minimum_water_change_interval = models.IntegerField(default=1, blank=False) #days
 
     #WATER REGULATION
-    auto_pump = models.BooleanField(default=False, null=False)
-    auto_refill = models.BooleanField(default=False, null=False)
-    resevoir_volume = models.IntegerField(default=27, null=False)  # liters
-    basin_volume = models.IntegerField(default=27, null=False)  # liters
+    auto_pump = models.BooleanField(default=False, blank=False)
+    auto_refill = models.BooleanField(default=False, blank=False)
+    resevoir_volume = models.IntegerField(default=27, blank=False)  # liters
+    basin_volume = models.IntegerField(default=27, blank=False)  # liters
 
     #DATA
-    min_change_interval = models.IntegerField(default=60, null=False)  # minutes
-    polling_range = models.IntegerField(default=60, null=False)  #minutes
-    minimun_data_count = models.IntegerField(default=100, null=False)
+    min_change_interval = models.IntegerField(default=60, blank=False)  # minutes
+    polling_range = models.IntegerField(default=60, blank=False)  #minutes
+    minimun_data_count = models.IntegerField(default=100, blank=False)
