c510931f04199c8bbfe76acc6ef9ef9a60454d7a
Preston Sheppard <psheppard16@gmail.com>
2018-07-14 21:09:19 -0400

    updated packages and js workflow

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,41 +2,11 @@ import $ from "jquery";
 import jQuery from "jquery";
 window.$ = $;
 window.jQuery = jQuery;
-require("noty");
 
-require("bootstrap/dist/js/bootstrap.bundle.js");
-require("bootstrap-datepicker/dist/js/bootstrap-datepicker");
-require("bootstrap-slider/dist/bootstrap-slider");
-require("jscrollpane");
+import {Global} from "./global.js";
 
-$(document).ready(() => {
-    //initialize datepicker ex.
-    // $(".datepicker").datepicker({
-    //     orientation: "bottom auto"
-    // });
+$(document).ready(function() {
+    "use strict";
 
-    let toTime = function(decimal) {
-        let hrs = parseInt(Number(decimal));
-        let min = Math.round((Number(decimal) - hrs) * 60);
-        let am = true;
-        if(hrs > 12) {
-            hrs -= 12;
-            am = false;
-        }
-        hrs = String(hrs);
-        min = String(min);
-        if (min.length === 1) min += "0";
-        if(am) return hrs + ":" + min + " AM";
-        else return hrs + ":" + min + " PM";
-    };
-
-    $(".data-slider").each(function () {
-        $(this).slider({id: this.id, range: true, handle: "square", tooltip: "hide"});
-        $(this).on("slide", function(slideEvt) {
-            let val_1 = $("#" + "id_low_" + this.id.split("_")[1]);
-            let val_2 = $("#" + "id_high_" + this.id.split("_")[1]);
-            val_1.attr("value", slideEvt.value[0]);
-            val_2.attr("value", slideEvt.value[1]);
-        });
-    });
+    new Global();
 });
